d349b7f64f605239c7ad2912f57450f6ac642683
18mward <58235396+18mward@users.noreply.github.com>
2023-05-07 15:00:41 -0600

    Update double_pendulum_tests.py

--- double_pendulum_tests.py
+++ double_pendulum_tests.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
 
 import numpy as np
 from double_pendulum import double_pendulum
@@ -10,6 +8,7 @@ from double_pendulum import double_pendulum
 # Test 1: Test the output shape of the double pendulum function
 t, y = double_pendulum(10, 0.01, [np.pi/2, np.pi/2, 0, 0])
 assert y.shape == (1000, 4)
+
 # Test 2: Test that the maximum value of t remains intact
 assert t[-1] == 10
 
@@ -21,10 +20,4 @@ assert np.allclose(y[0], [np.pi/2, np.pi/2, 0, 0])
 t, y1 = double_pendulum(10, 0.01, [np.pi/2, np.pi/2, 0, 0])
 t, y2 = double_pendulum(10, 0.01, [np.pi/2+0.01, np.pi/2+0.01, 0, 0])
 assert not np.allclose(y1[-1], y2[-1], atol=1e-1)
-
-
-# In[ ]:
-
-
-
 
